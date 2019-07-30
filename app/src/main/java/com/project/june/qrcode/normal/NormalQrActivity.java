38463811369e866931ecc4f1b6909dc02ed9bf2d
June <junemvp@foxmail.com>
2019-07-30 19:12:50 +0800

    初始化主页面

---
+++ app/src/main/java/com/project/june/qrcode/normal/NormalQrActivity.java
@@ -0,0 +1,15 @@
+package com.project.june.qrcode.normal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.june.qrcode.R;
+
+public class NormalQrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal_qr);
+    }
+}
